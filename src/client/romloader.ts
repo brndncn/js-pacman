289a8e4f9a862ee8419f3428faeae43191a0f46d
Brandon Canfield <sneelhorses@gmail.com>
2020-01-04 15:10:43 -0600

    Fix race condition in romloader
    Make sure palettes are loaded before tiles and sprites

--- src/client/romloader.ts
+++ src/client/romloader.ts
@@ -7,70 +7,78 @@ zip.useWebWorkers = false;
 
 export function loadRoms(rompack: Blob): void {
   zip.createReader(new zip.BlobReader(rompack), function(reader) {
-    reader.getEntries(function(entries) {
+    reader.getEntries((entries) => {
+
       entries.forEach((entry) => {
         // TODO DEBUG
         console.log(entry.filename, '0x' + entry.crc32.toString(16), entry.uncompressedSize);
-
-        if (entry.filename.endsWith('82s126.4a') && entry.uncompressedSize == 256) {
-          entry.getData(new zip.BlobWriter(), function(blob) {
-            blob.arrayBuffer().then( (buffer) => {
-              Graphics.loadPalettes(new Uint8Array(buffer));
-            });
-          });
-        }
       });
-      // TODO this ordering doesn't actually work, its still a race
-      entries.forEach((entry) => {
 
-        if (entry.filename.endsWith('5e') && entry.uncompressedSize == 4096) {
-          entry.getData(new zip.BlobWriter(), function(blob) {
-            blob.arrayBuffer().then( (buffer) => {
-              Graphics.loadTiles(new Uint8Array(buffer));
+      const palettesLoadedPromise = new Promise((resolve, reject) => {
+        entries.forEach((entry) => {
+          if (entry.filename.endsWith('82s126.4a') && entry.uncompressedSize == 256) {
+            entry.getData(new zip.BlobWriter(), function(blob) {
+              blob.arrayBuffer().then( (buffer) => {
+                Graphics.loadPalettes(new Uint8Array(buffer));
+                resolve();
+              });
             });
-          });
-        }
+          }
+        });
+      });
 
-        if (entry.filename.endsWith('5f') && entry.uncompressedSize == 4096) {
-          entry.getData(new zip.BlobWriter(), function(blob) {
-            blob.arrayBuffer().then( (buffer) => {
-              Graphics.loadSprites(new Uint8Array(buffer));
+      palettesLoadedPromise.then(() => {
+        entries.forEach((entry) => {
+
+          if (entry.filename.endsWith('5e') && entry.uncompressedSize == 4096) {
+            entry.getData(new zip.BlobWriter(), function(blob) {
+              blob.arrayBuffer().then( (buffer) => {
+                Graphics.loadTiles(new Uint8Array(buffer));
+              });
             });
-          });
-        }
+          }
 
-        if (entry.filename.endsWith('6e') && entry.uncompressedSize == 4096) {
-          entry.getData(new zip.BlobWriter(), function(blob) {
-            blob.arrayBuffer().then( (buffer) => {
-              Emulator.copyRom(0x0000, new Uint8Array(buffer));
+          if (entry.filename.endsWith('5f') && entry.uncompressedSize == 4096) {
+            entry.getData(new zip.BlobWriter(), function(blob) {
+              blob.arrayBuffer().then( (buffer) => {
+                Graphics.loadSprites(new Uint8Array(buffer));
+              });
             });
-          });
-        }
+          }
 
-        if (entry.filename.endsWith('6f') && entry.uncompressedSize == 4096) {
-          entry.getData(new zip.BlobWriter(), function(blob) {
-            blob.arrayBuffer().then( (buffer) => {
-              Emulator.copyRom(0x1000, new Uint8Array(buffer));
+          if (entry.filename.endsWith('6e') && entry.uncompressedSize == 4096) {
+            entry.getData(new zip.BlobWriter(), function(blob) {
+              blob.arrayBuffer().then( (buffer) => {
+                Emulator.copyRom(0x0000, new Uint8Array(buffer));
+              });
             });
-          });
-        }
+          }
 
-        if (entry.filename.endsWith('6h') && entry.uncompressedSize == 4096) {
-          entry.getData(new zip.BlobWriter(), function(blob) {
-            blob.arrayBuffer().then( (buffer) => {
-              Emulator.copyRom(0x2000, new Uint8Array(buffer));
+          if (entry.filename.endsWith('6f') && entry.uncompressedSize == 4096) {
+            entry.getData(new zip.BlobWriter(), function(blob) {
+              blob.arrayBuffer().then( (buffer) => {
+                Emulator.copyRom(0x1000, new Uint8Array(buffer));
+              });
             });
-          });
-        }
+          }
 
-        if (entry.filename.endsWith('.6j') && entry.uncompressedSize == 4096) {
-          entry.getData(new zip.BlobWriter(), function(blob) {
-            blob.arrayBuffer().then( (buffer) => {
-              Emulator.copyRom(0x3000, new Uint8Array(buffer));
+          if (entry.filename.endsWith('6h') && entry.uncompressedSize == 4096) {
+            entry.getData(new zip.BlobWriter(), function(blob) {
+              blob.arrayBuffer().then( (buffer) => {
+                Emulator.copyRom(0x2000, new Uint8Array(buffer));
+              });
             });
-          });
-        }
+          }
 
+          if (entry.filename.endsWith('.6j') && entry.uncompressedSize == 4096) {
+            entry.getData(new zip.BlobWriter(), function(blob) {
+              blob.arrayBuffer().then( (buffer) => {
+                Emulator.copyRom(0x3000, new Uint8Array(buffer));
+              });
+            });
+          }
+
+        });
       });
     });
   });
